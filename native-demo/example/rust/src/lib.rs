07a878244ed8386bdadcc723ed84c2931a9edf26
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-03-27 14:55:26 +0800

    remove useless files
    just remove lots of C files, for there are not working.

--- native-demo/example/rust/src/lib.rs
+++ native-demo/example/rust/src/lib.rs
@@ -45,3 +45,20 @@ pub mod android {
         output.into_inner()
     }
 }
+
+use log::info;
+use ndk::trace;
+
+#[cfg_attr(target_os = "android",
+    ndk_glue::main( backtrace = "on",
+        // logger(level = "debug", tag = "hello-world"),
+    )
+)]
+pub fn main() {
+    // println!("hello world");
+    let _trace;
+    if trace::is_trace_enabled() {
+        _trace = trace::Section::new("ndk-rs example main").unwrap();
+    }
+    info!("hello world");
+}
