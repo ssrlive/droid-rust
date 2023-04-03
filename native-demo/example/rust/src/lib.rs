a13b47335a8407bbd2a68909fb4545cac98e1391
ssrlive <30760636+ssrlive@users.noreply.github.com>
2023-04-03 20:56:49 +0800

    update rust code.

--- native-demo/example/rust/src/lib.rs
+++ native-demo/example/rust/src/lib.rs
@@ -1,17 +1,38 @@
-use log::info;
-use ndk::trace;
+#[cfg(target_os = "android")]
+#[no_mangle]
+fn android_main(app: android_activity::AndroidApp) {
+    use android_activity::{InputStatus, MainEvent, PollEvent};
 
-#[cfg_attr(target_os = "android",
-    ndk_glue::main(
-        backtrace = "on",
-        logger(level = "debug", tag = "sample-tag"),
-    )
-)]
-pub fn main() {
-    // println!("hello world");
-    let _trace;
-    if trace::is_trace_enabled() {
-        _trace = trace::Section::new("ndk-rs example main").unwrap();
+    android_logger::init_once(
+        android_logger::Config::default().with_max_level(log::LevelFilter::Info),
+    );
+
+    loop {
+        let timeout = Some(std::time::Duration::from_millis(1000));
+        app.poll_events(timeout, |event| {
+            match event {
+                PollEvent::Wake => {
+                    log::info!("Early wake up");
+                }
+                PollEvent::Timeout => {
+                    log::info!("hello world 你好世界.");
+                }
+                PollEvent::Main(main_event) => {
+                    log::info!("Main event: {:?}", main_event);
+                    match main_event {
+                        MainEvent::Destroy => {
+                            return;
+                        }
+                        _ => {}
+                    }
+                }
+                _ => {}
+            }
+
+            app.input_events(|event| {
+                log::info!("Input Event: {event:?}");
+                InputStatus::Unhandled
+            });
+        });
     }
-    info!("hello world 你好世界");
 }
